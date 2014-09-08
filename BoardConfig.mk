46bdfbfde67b898bc0f798b9b61fe420fd6a1ca3
bigsupersquid <0.uradork.0@gmail.com>
2014-09-08 12:22:55 -0500

    bringup

---
+++ BoardConfig.mk
@@ -0,0 +1,40 @@
+-include vendor/lge/thunder-common/BoardConfigVendor.mk
+-include device/lge/msm7x27-common/BoardConfigCommon.mk
+
+TARGET_KERNEL_SOURCE 				:= kernel/lge/msm7x27-3.0.x-p500
+
+MINIGZIP 							:= $(shell which lzma)
+ARM_EABI_TOOLCHAIN 					:= $(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/arm-unknown-eabi-4.7/bin
+#ARM_EABI_TOOLCHAIN 				:= $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/bin 
+TARGET_GCC_VERSION_AND 				:= 4.7-sm
+
+## Boot loader & recovery
+SKIP_SET_METADATA 					:= true
+TARGET_RECOVERY_FSTAB 				:= device/lge/thunder-common/recovery/recovery.fstab
+
+DEVICE_RESOLUTION 					:= 320x480
+RECOVERY_GRAPHICS_USE_LINELENGTH 	:= true
+HAVE_SELINUX 						:= true
+BOARD_HAS_NO_SELECT_BUTTON 			:= true
+BOARD_USE_CUSTOM_RECOVERY_FONT 		:= \"font_7x16.h\"
+TARGET_RECOVERY_PIXEL_FORMAT 		:= "RGB_565"
+SMALLER_FONT_FOOTPRINT 				:= true
+TARGET_NO_SEPARATE_RECOVERY 		:= true
+
+TARGET_NO_INITLOGO 					:= true
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH 	:= \"/sys/class/leds/lcd-backlight/brightness\"
+TARGET_USERIMAGES_USE_F2FS 			:= true
+
+TW_BRIGHTNESS_PATH 					:= /sys/class/leds/lcd-backlight/brightness
+TW_EXCLUDE_SUPERSU 					:= true
+TW_EXCLUDE_ENCRYPTED_BACKUPS 		:= true
+TW_MAX_BRIGHTNESS 					:= 255
+TW_NO_SCREEN_BLANK 					:= 
+TW_NO_REBOOT_BOOTLOADER 			:= false
+TW_CUSTOM_POWER_BUTTON 				:= 107
+
+#Camera
+#TARGET_SPECIFIC_HEADER_PATH 		:= device/lge/thunder-common/include3x 
+TARGET_CAMERA_SENSOR_MP_SIZE 		:= 3
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT -DZTE_CAMERA_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB -DNEEDS_VECTORIMPL_SYMBOLS
---
+++ README.md
@@ -0,0 +1,1 @@
+Thunderg and thunderc are very similar, here are more shared parts that differ from msm7x27-common.
